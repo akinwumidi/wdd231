3d70195de61ed953e18a4d95307930d76e99f840
Israel Akinwumi <israelakin14@gmail.com>
2026-07-07 21:12:48 -0600

    implement current year and last modified logic

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+  // Set current year and last modified date in footer logic
+  document.getElementById('currentyear').textContent = new Date().get;
+  document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
